403d0722d55c31b6b5ec1847325a63129999b3ce
kkgdll <64827562+kkgdll@users.noreply.github.com>
2020-07-23 23:23:37 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/ll/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.ll.git;
+
+/**
+ * @author LiuLong
+ * @date 2020/7/23 - 23:11
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
